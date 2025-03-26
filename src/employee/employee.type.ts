6a9a14f54c8dc9b21a29a769df2641adf6480320
benny48 <arvan.benny@gmail.com>
2025-03-26 13:56:26 +0700

    tes aja mbuh

--- src/employee/employee.type.ts
+++ src/employee/employee.type.ts
@@ -32,7 +32,7 @@ export class Employee {
   @Field(() => Boolean)
   magang: boolean;
 
-  @Field(() => [KPI]) // ini test mbuhh
+  @Field(() => [KPI]) // ini test mbuhhlah
   kpi_id: KPI[]; // Ubah menjadi array KPI, bukan String[]
 
   @Field(() => String, { nullable: true })
