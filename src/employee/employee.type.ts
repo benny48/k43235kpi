5e31d48eb9fa4e20bef2546aef6646b34000562f
benny48 <arvan.benny@gmail.com>
2025-03-26 10:37:59 +0700

    tes aja lagi

--- src/employee/employee.type.ts
+++ src/employee/employee.type.ts
@@ -32,7 +32,7 @@ export class Employee {
   @Field(() => Boolean)
   magang: boolean;
 
-  @Field(() => [KPI]) // Perubahan di sini ok
+  @Field(() => [KPI]) // Perubahan di sini ok lah
   kpi_id: KPI[]; // Ubah menjadi array KPI, bukan String[]
 
   @Field(() => String, { nullable: true })
